3ced74c8200b698f610b2f4a122350be9d4ae8bb
ThiagoLMattos <thiago.mattos3@etec.sp.gov.br>
2026-03-17 09:56:11 -0300

    Second Commit

---
+++ frontend/MeuCrud/src/screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState } from "react";
+import { View, Text, FlatList, Button } from "react-native";
+
+import styles from "../styles/styles";
+
+import { getPeople, deletePerson } from "../servers/peopleCrud";
+
+export default function HomeScreen({ navigation }) {
+
+    const [people, setPeople] = useState([]);
+
+    async function loadPeople(){
+
+        const data = await getPeople();
+
+        setPeople(data);
+    }
+
+    useEffect(() =>{
+        loadPeople();
+    },[]);
+
+
+    return(
+
+        <View style={styles.container}>
+
+            <Text style={styles.title}>Pessoas</Text>
+
+            <Button
+                title="Adicionar Pessoa"
+                onPress={() => navigation.navigate("AddEdit")}
+            />
+
+            <FlatList
+                data={people}
+                keyExtractor = {(item) => item.id.toString()}
+
+                renderItem = {({item}) => (
+                    <CardPersonal
+                        item={item}
+                        navigation={navigation}
+                        refresh={loadPeople}
+                    />
+                )}
+            />
+        </View>
+    );
+}
