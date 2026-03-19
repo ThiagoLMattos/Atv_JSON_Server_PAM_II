784120a27ab083099f8c4ed056339b36aeb1316e
ThiagoLMattos <thiago.mattos3@etec.sp.gov.br>
2026-03-19 12:46:50 -0300

    Commit
    Started designing it to be a BBB inspired app 😊

--- frontend/MeuCrud/src/screens/HomeScreen.js
+++ frontend/MeuCrud/src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text, FlatList, Button } from "react-native";
 
 import styles from "../styles/styles";
 
-import { getPeople, deletePerson } from "../servers/peopleCrud";
+import { getPeople, deletePerson } from "../servers/participantsCrud";
 
 export default function HomeScreen({ navigation }) {
 
